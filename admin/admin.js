571a4985d87b07190fe2d94fee796b9f481b75fa
KDClothing <KDClothing32@gmail.com>
2025-05-05 22:52:32 +0530

    Add files via upload

---
+++ admin/admin.js
@@ -0,0 +1,7 @@
+
+function uploadProduct() {
+  document.getElementById("success-modal").classList.remove("hidden");
+  setTimeout(() => {
+    document.getElementById("success-modal").classList.add("hidden");
+  }, 2000);
+}
